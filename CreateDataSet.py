f6e830267c708c4ab6c1c79a2669f5cf6aa5a5bb
mmtalaie <m.m.talaie@live.com>
2021-03-23 23:45:42 +0430

    Create directory if not exist and Add multi running file

--- CreateDataSet.py
+++ CreateDataSet.py
@@ -7,7 +7,12 @@ textDirectory = "text/ArmanPersoNERCorpus"
 txtAddress = glob.glob(textDirectory+"/*.txt")
 print(txtAddress)
 composer = Compositor()
-fileDict = open("output/gt.txt", "a")
+fileDict = open("output/gt1.txt", "a")
+ingsDirectory = "output/imgs1"
+try:
+    os.stat(ingsDirectory)
+except:
+    os.mkdir(ingsDirectory)
 a = 0
 while(len(txtAddress) > 0):
     f = open(txtAddress.pop(0))
@@ -16,13 +21,13 @@ while(len(txtAddress) > 0):
         if not line:
             break
         words = line.split(" ")
-        if(words[0] =="ـ"):
+        if(words[0] == "ـ"):
             continue
         color = (int(random.random() * 255), int(random.random()
                  * 255), int(random.random() * 255), 255)
         img = composer.productImage(words[0], True, color, border=3)
-        img.save("output/imgs/word_"+str(a)+".png")
-        fileDict.write("imgs/word_"+str(a)+".png\t"+words[0]+"\n")
+        img.save(ingsDirectory + "/word_" + str(a) + ".png")
+        fileDict.write(ingsDirectory + "/word_"+str(a)+".png\t"+words[0]+"\n")
         fileDict.flush()
         a += 1
         print("imgs/word_"+str(a)+".png\t"+words[0]+"\n")
---
+++ CreateDatasetMultiRunning.py
@@ -0,0 +1,33 @@
+import os
+from Compositor import *
+import glob
+import random
+
+textDirectory = "text/ArmanPersoNERCorpus"
+txtAddress = glob.glob(textDirectory+"/*.txt")
+print(txtAddress)
+composer = Compositor()
+fileDict = open("output/gt1.txt", "a")
+ingsDirectory = "output/imgs1"
+try:
+    os.stat(ingsDirectory)
+except:
+    os.mkdir(ingsDirectory)
+a = 0
+
+f = open(txtAddress[0])
+while True:
+    line = f.readline()
+    if not line:
+        break
+    words = line.split(" ")
+    if(words[0] == "ـ"):
+        continue
+    color = (int(random.random() * 255), int(random.random()
+                * 255), int(random.random() * 255), 255)
+    img = composer.productImage(words[0], True, color, border=3)
+    img.save(ingsDirectory + "/word_" + str(a) + ".png")
+    fileDict.write(ingsDirectory + "/word_"+str(a)+".png\t"+words[0]+"\n")
+    fileDict.flush()
+    a += 1
+    print("imgs/word_"+str(a)+".png\t"+words[0]+"\n")
